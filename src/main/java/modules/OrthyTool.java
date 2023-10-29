cdcf09ca82951ff5fe2c6b472d5889d702e665a4
orthybt <orthybt@gmail.com>
2023-10-29 21:16:15 +0100

    Initial commit

---
+++ src/main/java/modules/OrthyTool.java
@@ -0,0 +1,12 @@
+package modules;
+
+import javafx.scene.image.Image;
+
+import java.awt.*;
+
+public interface OrthyTool {
+	String getName();
+	String getDescription();
+	Image getIcon();
+	void launchTool();
+}
